248c8d15c0c34b4b5a2e2d3f77e1bbbaa7adc50a
Jasper Timmer <j.timmer@love2eat.eu>
2022-09-24 15:11:28 +0200

    remove KaTeX

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,9 +1,6 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const math = require('remark-math');
-const katex = require('rehype-katex');
-
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
@@ -28,11 +25,6 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl: 'https://github.com/usethesource/rascal/tree/main/src/org/rascalmpl/courses/',
-          remarkPlugins: [
-            // codeBlock,
-            math
-          ],
-          rehypePlugins: [katex],
         },
         blog: {
           showReadingTime: true,
@@ -40,26 +32,12 @@ const config = {
           blogSidebarCount: 'ALL',
           // Please change this to your repo.
           editUrl: 'https://github.com/usethesource/rascal-website/tree/website-v2/blog/',
-          remarkPlugins: [
-            // codeBlock,
-            math
-          ],
-          rehypePlugins: [katex],
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
-  ],
-  stylesheets: [
-    {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
-      integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
-    },
   ],
   plugins: [
     [
